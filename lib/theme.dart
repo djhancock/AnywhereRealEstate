c417bcd7124b63513ee27fc67e582e36b6697a54
Jason Hancock <djhancock@gmail.com>
2023-08-06 10:50:15 -0500

    fixed theme not applying properly

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simpsons_demo/flavor.dart';
 
-ThemeData appTheme(BuildContext context) => ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-    useMaterial3: true,
+ThemeData appTheme(BuildContext context) {
+  final flavor = Provider.of<Flavor>(context);
+
+  switch (flavor) {
+    case Flavor.simpsons:
+      return _getSimpsonsTheme(context);
+    case Flavor.theWire:
+      return _getTheWireTheme(context);
+  }
+}
+
+ThemeData _getSimpsonsTheme(BuildContext context) {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF3700b3)),
     toggleButtonsTheme: ToggleButtonsThemeData(
-        selectedColor: Colors.blue,
-        color: Colors.blue.withAlpha(128),
-        textStyle: const TextStyle(
-          color: Colors.white,
-        )));
+      selectedColor: Colors.blue,
+      color: Colors.blue.withAlpha(128),
+      textStyle: const TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
+
+ThemeData _getTheWireTheme(BuildContext context) {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF027A1C)),
+    toggleButtonsTheme: ToggleButtonsThemeData(
+      selectedColor: Colors.blue,
+      color: Colors.blue.withAlpha(128),
+      textStyle: const TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
